458446ff99c1c029719c12ae2f21beaff5c23fd4
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-14 23:01:14 +0500

    Update : import ReactiveForm Module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import * as Material from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class MaterialModule {}
